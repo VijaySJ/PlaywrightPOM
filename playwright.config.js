0ff30f60e0f04b05008f6f63e8779346f45e63a5
VijaySJ <vijaysjofficial@gmail.com>
2025-05-19 01:49:41 +0530

    hometest changes

--- playwright.config.js
+++ playwright.config.js
@@ -1,24 +1,24 @@
+// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-module.exports = {
+export default defineConfig({
   testDir: './src/tests',
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
-  // globalSetup: require.resolve('./src/config/global-setup'), // Correct globalSetup path
   use: {
-    // storageState: './storageState.json', // Use this session state file
-    headless: false,
-    viewport: { width: 1280, height: 720 },
-    baseURL: 'https://opensource-demo.orangehrmlive.com',
+    baseURL: 'https://opensource-demo.orangehrmlive.com/',
     trace: 'on-first-retry',
+    headless: false, // Enables headless mode
   },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'], 
+          },
     },
   ],
-};
+});
